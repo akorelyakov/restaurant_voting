f81d4aa4893b9fe18d46966e24ea327d83201613
akorelyakov <al.korelyakov@gmail.com>
2021-03-11 15:46:45 +0300

    add models

---
+++ src/main/java/com/korelyakov/restaurant_voting/model/AbstractBaseEntity.java
@@ -0,0 +1,31 @@
+package com.korelyakov.restaurant_voting.model;
+
+public class AbstractBaseEntity {
+
+    protected Integer id;
+
+    protected AbstractBaseEntity() {
+    }
+
+    protected AbstractBaseEntity(Integer id) {
+        this.id = id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    // doesn't work for hibernate lazy proxy
+    public int id() {
+        //Assert.notNull(id, "Entity must have id");
+        return id;
+    }
+
+    boolean isNew() {
+        return getId() == null;
+    }
+}
---
+++ src/main/java/com/korelyakov/restaurant_voting/model/Role.java
@@ -0,0 +1,6 @@
+package com.korelyakov.restaurant_voting.model;
+
+public enum Role {
+    USER,
+    ADMIN
+}
