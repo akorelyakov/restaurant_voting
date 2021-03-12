ad8beaf3842738c02351f5887b77ee2b0173f9d7
akorelyakov <al.korelyakov@gmail.com>
2021-03-12 16:04:54 +0300

    add spring-app.xml

---
+++ src/main/java/com/korelyakov/restaurant_voting/SpringMain.java
@@ -0,0 +1,14 @@
+package com.korelyakov.restaurant_voting;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Arrays;
+
+public class SpringMain {
+    public static void main(String[] args) {
+        try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml")) {
+            System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
+        }
+    }
+}
